98a5d3368ec3c21182f29c41c54ddd30be84d478
chenzong24635 <chenzong24635@163.com>
2019-03-08 09:45:07 +0800

    a

---
+++ src/pages/login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="">
+登录
+  </div>
+</template>
+<script>
+export default {
+  name: '',
+  components: {},
+  data () {
+    return {}
+  },
+  created () {},
+  methods: {}
+}
+</script>
+<style scoped lang="less">
+</style>
